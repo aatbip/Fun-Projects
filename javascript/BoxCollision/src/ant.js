a8d6b956dbeccaf38048b6be5ad12f88f4850c96
aatbip <anantabipal2010@gmail.com>
2021-12-28 04:44:43 +0545

    [add] BoxCollision

---
+++ javascript/BoxCollision/src/ant.js
@@ -0,0 +1,41 @@
+function Ant() {
+  this.dx = 1;
+  this.dy = 1;
+
+  this.box = document.createElement("div");
+  this.box.classList.add("ant");
+}
+
+Ant.prototype.init = function (x, y, radius) {
+  this.x = x;
+  this.y = y;
+  // this.dx = 1;
+  // this.dy = 1;
+  this.radius = radius;
+  this.speed = 1;
+  this.width = this.radius * 2;
+  this.height = this.radius * 2;
+  // this.box = document.createElement("div");
+  this.box.style.width = this.width + "px";
+  this.box.style.height = this.height + "px";
+  this.box.style.position = "absolute";
+  this.box.style.left = this.x + "px";
+  this.box.style.top = this.y + "px";
+  this.box.style.backgroundImage = 'url("./images/ant.gif")';
+  this.box.style.backgroundSize = this.width + "px " + this.height + "px";
+  // this.box.classList.add("ant");
+};
+// }
+Ant.prototype.draw = function (canvas) {
+  canvas.appendChild(this.box);
+  let that = this;
+  this.box.addEventListener("click", function () {
+    that.box.parentElement.removeChild(that.box);
+  });
+};
+
+Ant.prototype.move = function () {
+  this.box.style.left = this.x + "px";
+  this.box.style.top = this.y + "px";
+};
+// }
